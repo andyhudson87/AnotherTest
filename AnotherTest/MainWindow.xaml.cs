5d4175e09509be0d5aefd7e412ab0b3cfba6ea9e
Big John <big.john@bigjohn.john>
2015-09-30 17:04:29 +0100

    Change 1

---
+++ AnotherTest/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnotherTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person _person;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _person = new Person();
+            textBoxFirstName.DataContext = _person;
+        }
+
+        private void buttonSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            _person.FirstName = "Big John";
+        }
+    }
+}
---
+++ AnotherTest/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace AnotherTest
+{
+    class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("FirstName"));
+                }
+            }
+        }
+
+    }
+}
