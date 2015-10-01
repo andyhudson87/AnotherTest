af8aa3ed2abe9b315939daa74f1ba3dd5b8846e4
Big John <big.john@bigjohn.john>
2015-10-01 10:15:50 +0100

    Populate button replaced submit button. Cancel button removed. Surname, Email properties added to Person class.

--- AnotherTest/MainWindow.xaml.cs
+++ AnotherTest/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AnotherTest
 {
@@ -26,11 +13,15 @@ namespace AnotherTest
             InitializeComponent();
             _person = new Person();
             textBoxFirstName.DataContext = _person;
+            textBoxSurname.DataContext = _person;
+            textBoxEmail.DataContext = _person;
         }
 
-        private void buttonSubmit_Click(object sender, RoutedEventArgs e)
+        private void buttonPopulate_Click(object sender, RoutedEventArgs e)
         {
             _person.FirstName = "Big John";
+            _person.Surname = "Hudson";
+            _person.Email = "big.john.hudson@tvsscs.com";
         }
     }
 }
--- AnotherTest/Person.cs
+++ AnotherTest/Person.cs
@@ -1,30 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 
 namespace AnotherTest
 {
     class Person : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-
         private string _firstName;
-
         public string FirstName
         {
             get { return _firstName; }
             set
             {
-                _firstName = value;
-                if (PropertyChanged != null)
+                if (!_firstName.Equals(value))
                 {
-                    PropertyChanged(this, new PropertyChangedEventArgs("FirstName"));
+                    _firstName = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("FirstName"));
+                    }
                 }
             }
         }
 
+        private string _surname;
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                if (!_surname.Equals(value))
+                {
+                    _surname = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Surname"));
+                    }
+                }
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (!_email.Equals(value))
+                {
+                    _email = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+                    }
+                }
+            }
+        }
+
+        public Person(string firstName = "", string surname = "", string email = "")
+        {
+            _firstName = firstName;
+            _surname = surname;
+            _email = email;
+        }
     }
 }
