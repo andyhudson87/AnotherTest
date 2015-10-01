dc4340defe4d14c85246146c19f9e49e701c18c4
Big John <big.john@bigjohn.john>
2015-10-01 15:56:29 +0100

    Added post-deploy script and entity framework

---
+++ MyDatabase/Script.PostDeploymentPopulate.sql
@@ -0,0 +1,27 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+MERGE INTO Person AS Target 
+USING (VALUES 
+  ('John', 'Big Man', 'john@bigjohn.com'), 
+  ('Andrew', 'Hudson', 'andrew.hudson@tvsscs.com'), 
+  ('Mike', 'Moore', ''), 
+  ('Sam', 'Loughlin', 'sam.loughlin@tvsscs.com') 
+) 
+AS Source (first_name, surname, email) 
+ON Target.first_name = Source.first_name and Target.surname = Source.surname
+-- update matched rows 
+WHEN MATCHED THEN 
+UPDATE SET first_name = Source.first_name, surname = Source.surname, email = Source.email 
+-- insert new rows 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (first_name, surname, email) 
+VALUES (first_name, surname, email);
