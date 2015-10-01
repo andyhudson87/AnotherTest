0d5c0214f543ab3a86f52bea4617ae8d530ecabf
Big John <big.john@bigjohn.john>
2015-10-01 16:48:16 +0100

    Entity framework

--- AnotherTest/MainWindow.xaml.cs
+++ AnotherTest/MainWindow.xaml.cs
@@ -2,27 +2,24 @@
 using System.Reflection;
 
 namespace AnotherTest
-{
+{ // http://www.codeproject.com/Articles/873592/Tutorial-for-a-Basic-WPF-MVVM-Project-Using-Entity
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         Person _person;
+        DatabaseContext _context;
         public MainWindow()
         {
             InitializeComponent();
             _person = new Person();
-            textBoxFirstName.DataContext = _person;
-            textBoxSurname.DataContext = _person;
-            textBoxEmail.DataContext = _person;
+            _context = new DatabaseContext();
         }
 
         private void buttonPopulate_Click(object sender, RoutedEventArgs e)
         {
-            _person.FirstName = "Big John";
-            _person.Surname = "Hudson";
-            _person.Email = "big.john.hudson@tvsscs.com";
+            _context.Persons.Add(new Person("John", "Big", "big.john@john.com"));
         }
     }
 }
